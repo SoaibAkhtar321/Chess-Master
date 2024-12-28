3b05f280866c0aa18a86a49a89515f914f73f1fb
SoaibAkhtar321 <akhtarsoaib568@gmail.com>
2024-12-29 00:06:47 +0530

    Update UserRegistrationServlet.java

--- src/com/chess/application/servlets/UserRegistrationServlet.java
+++ src/com/chess/application/servlets/UserRegistrationServlet.java
@@ -1,1 +1,32 @@
+package com.application.servlets; // Updated package name
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/userRegistration")
+public class UserRegistrationServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Welcome to User Registration Page</h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String email = request.getParameter("email");
+
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Registration Successful</h1>");
+        response.getWriter().println("<p>Username: " + username + "</p>");
+        response.getWriter().println("<p>Email: " + email + "</p>");
+    }
+}
