b3b566a20bbf52b50f7f0c18c552381abed9c11e
SoaibAkhtar321 <akhtarsoaib568@gmail.com>
2024-12-29 18:57:25 +0530

    Update ChessGameDAOTest.java

--- test/com/dao/ChessGameDAOTest.java
+++ test/com/dao/ChessGameDAOTest.java
@@ -1,1 +1,40 @@
+package com.chess.dao;
 
+import com.chess.model.GameState;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class ChessGameDAOTest {
+
+    @Autowired
+    private ChessGameDAO chessGameDAO;
+
+    @Test
+    void testSaveGame() {
+        // Arrange
+        GameState gameState = new GameState(); // Populate with test data
+
+        // Act
+        boolean isSaved = chessGameDAO.saveGame(gameState);
+
+        // Assert
+        assertTrue(isSaved);
+    }
+
+    @Test
+    void testGetGameState() {
+        // Arrange
+        GameState gameState = new GameState(); // Populate with test data
+        chessGameDAO.saveGame(gameState);
+
+        // Act
+        GameState retrievedState = chessGameDAO.getGameState();
+
+        // Assert
+        assertNotNull(retrievedState);
+    }
+}
